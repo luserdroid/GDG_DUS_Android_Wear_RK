11c571026ef3e20337e8a0cd83d8bcdee4e128a4
Resul <luser_k@hotmail.com>
2014-06-15 04:02:21 +0200

    Add pages to notifications

--- src/com/luser/android/wear/CareMaker.java
+++ src/com/luser/android/wear/CareMaker.java
@@ -4,6 +4,8 @@ import com.luser.android.wear.notification.NotificationManagerHelper;
 
 import android.app.Notification;
 import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat.BigTextStyle;
 
 public class CareMaker {
 
@@ -18,19 +20,23 @@ public class CareMaker {
 		NotificationManagerHelper notificationHelper = new NotificationManagerHelper(clzz,
 				context);
 
+		//contact
 		Notification notification = notificationHelper.createNotification(
 				R.drawable.ic_launcher, R.string.notification_contact,
-				R.string.notification_contact_caretaker,NotificationManagerHelper.GROUP_CONTACT);
+				R.string.notification_contact_caretaker,NotificationManagerHelper.GROUP_CONTACT, null);
 		notificationHelper.triggerNotification(notification, NotificationManagerHelper.CONTACT);
 
+		//emergency
+		Notification page = notificationHelper.createPage(R.string.notification_emergency_description_title, R.string.notification_emergency_description);
 		notification = notificationHelper.createNotification(R.drawable.ic_launcher,
 				R.string.notification_emergency,
-				R.string.notification_emergency_ambulance,NotificationManagerHelper.GROUP_EMERGENCY);
+				R.string.notification_emergency_ambulance,NotificationManagerHelper.GROUP_EMERGENCY, page);
 		notificationHelper.triggerNotification(notification, NotificationManagerHelper.EMERGENCY);
 		
+		//service
 		notification = notificationHelper.createNotification(R.drawable.ic_launcher,
 				R.string.notification_service,
-				R.string.notification_service_sanitary,NotificationManagerHelper.GROUP_SERVICE);
+				R.string.notification_service_sanitary,NotificationManagerHelper.GROUP_SERVICE, null);
 		notificationHelper.triggerNotification(notification, NotificationManagerHelper.SERVICE);
 	}
 }
--- src/com/luser/android/wear/notification/NotificationManagerHelper.java
+++ src/com/luser/android/wear/notification/NotificationManagerHelper.java
@@ -1,5 +1,7 @@
 package com.luser.android.wear.notification;
 
+import java.util.ArrayList;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -8,6 +10,7 @@ import android.content.Intent;
 import android.preview.support.wearable.notifications.WearableNotifications;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.app.NotificationCompat.BigTextStyle;
 
 /**
  * Provides and manages notifications
@@ -54,7 +57,22 @@ public class NotificationManagerHelper {
 		notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
 	}
 	
-	public Notification createNotification(int notificationIcon, int resIdtitle, int resIdContentText, String groupKey){
+	public Notification createPage(int idResTitle, int idPageResContentText){
+		// Create a big text style for the second page
+		BigTextStyle secondPageStyle = new NotificationCompat.BigTextStyle();
+		secondPageStyle.setBigContentTitle(context.getString(idResTitle))
+		               .bigText(context.getString(idPageResContentText));
+
+		// Create second page notification
+		Notification newPageNotification =
+		        new NotificationCompat.Builder(context)
+		        .setStyle(secondPageStyle)
+		        .build();
+		
+		return newPageNotification;
+	}
+	
+	public Notification createNotification(int notificationIcon, int resIdtitle, int resIdContentText, String groupKey, Notification page){
 		NotificationCompat.Builder notificationBuilder =
 		        new NotificationCompat.Builder(context)
 		        .setSmallIcon(notificationIcon)
@@ -64,14 +82,13 @@ public class NotificationManagerHelper {
 		        .setOngoing(true)
 		        .setContentText(context.getString(resIdContentText));
 		
-		Notification notification = new WearableNotifications.Builder(notificationBuilder)
-			.setGroup(groupKey)
-			.setHintHideIcon(true).build();
-		return notification;
-	}
-	
-	public void triggerNotifications(){
+		WearableNotifications.Builder featureBuilder = new WearableNotifications.Builder(notificationBuilder);
+		featureBuilder.setGroup(groupKey);
+		if(page != null){
+			featureBuilder.addPage(page);
+		}
 		
+		return notificationBuilder.build();
 	}
 	
 	public void triggerNotification(Notification notification, int id){
